e066d408bc996289832dca41a09db3fb4a3eff75
Mayque Oliveira <mayque.731@gmail.com>
2024-06-12 09:46:32 -0400

    testes adicionados, pedidos com erro

--- controllers/pedidos-controller.js
+++ controllers/pedidos-controller.js
@@ -46,129 +46,98 @@ exports.getPedidos = (req, res, next) => {
   });
 };
 
-exports.postPedidos = (req, res, next) => {
+exports.postPedidos = async(req, res, next) => {
   try {
+    const queryProduto = "SELECT * FROM produtos WHERE id_produto = ?";
+    const resultProduto = await mysql.execute(queryProduto, [req.body.id_produto]);
+
+    if (resultProduto.length == 0) {
+      return res.status(404).send({
+        mensagem: "Produto não encontrado",
+      });
+    }
+
+    const queryPedido = "INSERT INTO pedidos (quantidade, id_produto) VALUES (?,?)";
+    const resultPedido = await mysql.execute(queryPedido, [
+      req.body.quantidade,
+      req.body.id_produto,
+    ]);
+
+    const response = {
+      mensagem: "pedido inserido com sucesso",
+      produtoCriado: {
+        id_pedido: resultPedido.id_pedido,
+        id_produto: req.body.id_produto,
+        quantidade: req.body.quantidade,
+        request: {
+          tipo: "GET",
+          descricao: "Retorna todos pedidos",
+          url: "http://localhost:3000/pedidos",
+        },
+      },
+    };
+          res.status(201).send(response);
   } catch (error) {
     return res.status(500).send({ error: error });
-  }
-
-  mysql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error });
-    }
-    conn.query(
-      "SELECT * FROM produtos WHERE id_produto = ?",
-      [req.body.id_produto],
-      (error, resultado, field) => {
-        if (error) {
-          return res.status(500).send({ error: error });
-        }
-        if (resultado.length == 0) {
-          return res.status(404).send({
-            mensagem: "Produto não encontrado",
-          });
-        }
-        conn.query(
-          "INSERT INTO pedidos (quantidade, id_produto) VALUES (?,?)",
-          [req.body.quantidade, req.body.id_produto],
-          (error, resultado, field) => {
-            conn.release();
-            if (error) {
-              return res.status(500).send({ error: error });
-            }
-            const response = {
-              mensagem: "pedido inserido com sucesso",
-              produtoCriado: {
-                id_pedido: resultado.id_pedido,
-                id_produto: req.body.id_produto,
-                quantidade: req.body.quantidade,
-                request: {
-                  tipo: "GET",
-                  descricao: "Insere um pedido",
-                  url: "http://localhost:3000/pedidos",
-                },
-              },
-            };
-            res.status(201).send(response);
-          }
-        );
-      }
-    );
-  });
+  }  
 };
 
-exports.getUmPedido = (req, res, next) => {
+exports.getUmPedido = async(req, res, next) => {
   try {
+    const query = "SELECT * FROM pedidos WHERE id_pedidos = ?";
+    const result = await mysql.execute(query, [req.params.id_pedidos]);
+
+    if (result.length == 0) {
+      return res.status(404).send({
+        mensagem: "Não foi encontrado produto com este ID",
+      });
+    }
+
+    const response = {
+      pedido: {
+        id_pedido: result[0].id_pedido,
+        id_produto: result[0].id_produto,
+        quantidade: result[0].quantidade,
+        request: {
+          tipo: "GET",
+          descricao: "Retorna um pedido",
+          url: "http://localhost:3000/pedidos",
+        },
+      },
+    };
+    return res.status(201).send(response);
   } catch (error) {
     return res.status(500).send({ error: error });
-  }
-
-  mysql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error });
-    }
-    conn.query(
-      "SELECT * FROM pedidos WHERE id_pedidos = ?",
-      [req.params.id_pedidos],
-      (error, resultado, fields) => {
-        if (error) {
-          return res.status(500).send({ error: error });
-        }
-        if (resultado.length == 0) {
-          return res.status(404).send({
-            mensagem: "Não foi encontrado produto com este ID",
-          });
-        }
-        const response = {
-          pedido: {
-            id_pedidos: resultado[0].id_produto,
-            id_produto: resultado[0].id_produto,
-            quantidade: resultado[0].quantidade,
-            request: {
-              tipo: "GET",
-              descricao: "Retorna um pedido",
-              url: "http://localhost:3000/pedidos",
-            },
-          },
-        };
-        return res.status(201).send(response);
-      }
-    );
-  });
+  }  
 };
 
-exports.deletePedido = (req, res, next) => {
+exports.deletePedido = async(req, res, next) => {
   try {
+    const queryPedido = "SELECT * FROM pedidos WHERE id_pedidos = ?";
+    const result = await mysql.execute(queryPedido, [req.body.id_pedido]);
+
+    if (result.length == 0) {
+      return res.status(404).send({
+        mensagem: "Pedido não encontrado",
+      });
+    }
+
+    const query = "DELETE FROM pedidos WHERE id_pedidos = ?";
+    resultPedido = await mysql.execute(query, [req.body.id_pedido]);
+    const response = {
+      mensagem: "pedido removido com sucesso",
+      request: {
+        tipo: "POST",
+        descricao: "Insere um pedido",
+        url: "http://localhost:3000/pedidos",
+        body: {
+          id_produto: "Number",
+          quantidade: "Number",
+        },
+      },
+    };
+    return res.status(202).send({ response });
   } catch (error) {
     return res.status(500).send({ error: error });
   }
-
-  mysql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error });
-    }
-    conn.query(
-      `DELETE FROM pedidos WHERE id_pedidos = ?`,
-      [req.body.id_pedido],
-      (error, resultado, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error });
-        }
-        const response = {
-          mensagem: "pedido removido com sucesso",
-          request: {
-            tipo: "POST",
-            descricao: "Insere um pedido",
-            url: "http://localhost:3000/pedidos",
-            body: {
-              id_produto: "Number",
-              quantidade: "Number",
-            },
-          },
-        };
-        return res.status(202).send({ response });
-      }
-    );
-  });
 };
